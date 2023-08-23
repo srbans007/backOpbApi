66f2b301219bae12d379c1c0d179afd72b55469e
srbans007 <sr_bans007@hotmail.com>
2023-08-23 02:15:23 -0400

    time zone

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,7 +6,7 @@ config();
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
     host: process.env.DB_HOST!,
     dialect: 'mysql',
-    timezone: process.env.TIME_ZONE  // Utiliza la zona horaria desde el archivo .env
+    timezone: '+00:00'
 });
 
 export default sequelize;
